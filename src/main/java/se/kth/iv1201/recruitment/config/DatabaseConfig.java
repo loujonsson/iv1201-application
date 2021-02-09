546ba390a44fdc112c61095385585037221804d9
Veronika Nicolaou <vernic@kth.se>
2021-02-09 15:34:37 +0100

    still not working

--- src/main/java/se/kth/iv1201/recruitment/config/DatabaseConfig.java
+++ src/main/java/se/kth/iv1201/recruitment/config/DatabaseConfig.java
@@ -2,9 +2,28 @@ package se.kth.iv1201.recruitment.config;
 
 import org.springframework.context.annotation.Configuration;
 
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+
 @Configuration
 public class DatabaseConfig {
-    /*
+/*
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
+
+@Configuration
+public class DatabaseConfig {
+
     @Bean
     public BasicDataSource dataSource() throws URISyntaxException {
         String dbUrl = System.getenv("JDBC_DATABASE_URL");
