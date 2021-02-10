8473611d484f7ac0ad3a66d5ed3c18e1b05825f6
Veronika Nicolaou <vernic@kth.se>
2021-02-10 12:40:58 +0100

    excluded autoAuthSecurity

--- src/main/java/se/kth/iv1201/recruitment/Main.java
+++ src/main/java/se/kth/iv1201/recruitment/Main.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -22,7 +23,7 @@ import se.kth.iv1201.recruitment.repository.ApplicantRepository;
 // @EntityScan("se.kth.iv1201.recruitment.domain")
 // @EnableJpaRepositories(value = "se.kth.iv1201.recruitment.repository")
 // @RestController
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class Main {
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
 
