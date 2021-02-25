16e67984b6e010e55aadcd89ee9685b3d3b990f3
agnesaltin <43341692+agnesaltin@users.noreply.github.com>
2021-02-25 15:49:41 +0100

    .

--- src/main/java/se/kth/iv1201/recruitment/presentation/error/ExceptionHandlers.java
+++ src/main/java/se/kth/iv1201/recruitment/presentation/error/ExceptionHandlers.java
@@ -14,29 +14,8 @@ import org.slf4j.LoggerFactory;
 public class ExceptionHandlers implements ErrorController {
     Logger logger = LoggerFactory.getLogger(ExceptionHandlers.class);
 
-    @GetMapping("/error")
-    public String handleError(HttpServletRequest request){
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        System.out.println(status.toString());
 
-        if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
 
-            if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                //return "error-404";
-            }
-            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                //return "error-500";
-            }
-        }
-        return "error";
-    }
-
-    private String extractHttpStatusCode(HttpServletRequest request) {
-        return request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE).toString();
-    }
-
-    /*
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         //do something like logging
@@ -48,33 +27,16 @@ public class ExceptionHandlers implements ErrorController {
             //logger.info(statusCode.toString()); //statuskod alltid 404? Skumt...
 
             if(statusCode == HttpStatus.NOT_FOUND.value()) { //404
-                //do something like logging
-                //logger.info("A 404 not found exception occurred.");
+                logger.info("A 404 not found exception occurred.");
             }
             else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) { //500
-                //do something like logging
                 logger.info("A 500 internal server error exception occurred.");
             }
             else if(statusCode == HttpStatus.BAD_GATEWAY.value()) { //502
                 //do something like logging
             }
             else if(statusCode == HttpStatus.BAD_REQUEST.value()) { //400
-                //do something like logging
-            }
-            else if(statusCode == HttpStatus.FORBIDDEN.value()) { //403
-                //do something like logging
-            }
-            else if(statusCode == HttpStatus.MOVED_PERMANENTLY.value()) { //301
-                //do something like logging
-            }
-            else if(statusCode == HttpStatus.REQUEST_TIMEOUT.value()) { //408
-                //do something like logging
-            }
-            else if(statusCode == HttpStatus.TOO_MANY_REQUESTS.value()) { //429
-                //do something like logging
-            }
-            else if(statusCode == HttpStatus.UNAUTHORIZED.value()) { //401
-                //do something like logging
+                logger.info("A 400 bad request occurred.");
             }
             else {
                 //unspecified error
@@ -82,7 +44,7 @@ public class ExceptionHandlers implements ErrorController {
         }
 
         return "/error";
-    }*/
+    }
 
     /*
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
