b92df8afc17d72a4ca4443f8a992c32f40090a60
Veronika Nicolaou <vernic@kth.se>
2021-02-26 10:13:39 +0100

    added exception handling in recruitmentService

--- src/main/java/se/kth/iv1201/recruitment/presentation/error/ExceptionHandlers.java
+++ src/main/java/se/kth/iv1201/recruitment/presentation/error/ExceptionHandlers.java
@@ -3,18 +3,42 @@ package se.kth.iv1201.recruitment.presentation.error;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.kth.iv1201.recruitment.domain.IllegalRecruitmentTransactionException;
 
 @Controller
 public class ExceptionHandlers implements ErrorController {
+    public static final String ERROR_TYPE_KEY = "errorType";
+    public static final String GENERIC_ERROR = "generic";
+    public static final String SAVE_ACCOUNT_FAILED = "missing attribute";
+    public static final String LOGIN_FAILED = "login";
+
     Logger logger = LoggerFactory.getLogger(ExceptionHandlers.class);
 
-
+    /**
+     * Exception handler for broken business rules.
+     *
+     * @return An appropriate error page.
+     */
+    @ExceptionHandler(IllegalRecruitmentTransactionException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(IllegalRecruitmentTransactionException exception, Model model) {
+        logExceptionDebugLevel(exception);
+        if (exception.getMessage().toUpperCase().contains("MISSING ATTRIBUTE")) {
+            model.addAttribute(ERROR_TYPE_KEY, SAVE_ACCOUNT_FAILED);
+        } else if (exception.getMessage().toUpperCase().contains("LOGIN")) {
+            model.addAttribute(ERROR_TYPE_KEY, LOGIN_FAILED);
+        } else {
+            model.addAttribute(ERROR_TYPE_KEY, GENERIC_ERROR);
+        }
+        return "/error";
+    }
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
@@ -44,6 +68,10 @@ public class ExceptionHandlers implements ErrorController {
         }
 
         return "/error";
+    }
+
+    private void logExceptionDebugLevel(Exception exception) {
+        logger.debug("Exception handler got {}: {}", exception.getClass().getName(), exception.getMessage(), exception);
     }
 
     /*
