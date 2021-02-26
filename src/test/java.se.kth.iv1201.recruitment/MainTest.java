6993464597a19ce66b4fb3a6538bdde300211d37
loujons <loujons@kth.se>
2021-02-26 13:44:27 +0100

    removed outcommented stuff

--- src/test/java.se.kth.iv1201.recruitment/MainTest.java
+++ src/test/java.se.kth.iv1201.recruitment/MainTest.java
@@ -22,27 +22,10 @@ public class MainTest {
 
     @Autowired
     private MockMvc mockMvc;
-    
+
     @Test
     public void exampleTest() throws Exception {
         this.mockMvc.perform(get("/hello")).andDo(print()).andExpect(status().is(404));
     }
-
-/*    private Main main;
-
-    @BeforeEach
-    public void setUp() throws Exception{
-        main = new Main();
-    }
-    @Test
-    @DisplayName("Example test for CI/CD")
-    public void testForExampleTest(){
-        assertEquals(15, main.forExampleTest(5,10), "Regular sum works");
-    }
-
-    @Test
-    @DisplayName("Example test for CI/CD, negative")
-    public void testForExampleTestNeg(){
-        assertEquals(-10, main.forExampleTest(-1,-9), "Sum with negative integers");
-    }*/
+    
 }
