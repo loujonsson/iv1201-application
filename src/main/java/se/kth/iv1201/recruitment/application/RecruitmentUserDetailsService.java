a43d44e38db88be7339089bbac0867d3dafbcbf6
loujons <loujons@kth.se>
2021-03-06 11:54:07 +0100

    Authorization works!

--- src/main/java/se/kth/iv1201/recruitment/application/RecruitmentUserDetailsService.java
+++ src/main/java/se/kth/iv1201/recruitment/application/RecruitmentUserDetailsService.java
@@ -2,20 +2,27 @@ package se.kth.iv1201.recruitment.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import se.kth.iv1201.recruitment.domain.PersonDTO;
 
-import java.io.Serializable;
 import java.util.Collections;
-import java.util.List;
 
+/**
+ * Service for authentication and authorization.
+ */
 public class RecruitmentUserDetailsService implements UserDetailsService {
     @Autowired
     private RecruitmentService service;
 
+    /**
+     * Checks for username in database and applies, depending on what role_id the person in the db has, sets the role
+     *
+     * @param username Username of the person logging in
+     * @return User with username, his/her password and his/her role/(s)
+     * @throws UsernameNotFoundException
+     */
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         PersonDTO person = service.checkUsernameExists(username);
 
--- src/main/java/se/kth/iv1201/recruitment/config/WebSecurityConfig.java
+++ src/main/java/se/kth/iv1201/recruitment/config/WebSecurityConfig.java
@@ -6,53 +6,68 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import se.kth.iv1201.recruitment.application.RecruitmentService;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import se.kth.iv1201.recruitment.application.RecruitmentUserDetailsService;
+import se.kth.iv1201.recruitment.domain.RecruitmentSimpleUrlAuthenticationSuccessHandler;
 
+/**
+ * Configuration for authentication and authorization, using the framework Spring security.
+ */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private RecruitmentService service;
 
+    /**
+     * Configure the URLs that are supposed to be access and permitted to all users of the website
+     * Configures URL page for login
+     *
+     * @param http HTTP security of http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/create-account", "/error/*").permitAll() // these pages are configured to not require any authentication, all other paths must be authenticated
-                    .antMatchers("/admin").hasAuthority("ROLE_ADMIN")
-                    .antMatchers("/application").hasAuthority("ROLE_APPLICANT")
-                    .anyRequest().authenticated()
-                    .and()
+                .antMatchers("/", "/create-account", "/error/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
                 .formLogin()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/login-success.html", true)
-                    .permitAll()
-                    .and()
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .successHandler(myAuthenticationSuccessHandler())
+                .permitAll()
+                .and()
                 .logout()
-                    .permitAll();
+                .permitAll();
     }
 
+    /**
+     * A bean of RecruitmentUserDetailsService that handles the user details
+     *
+     * @return a custom UserDetailsService
+     */
     @Bean
     public RecruitmentUserDetailsService userDetailsService() {
         return new RecruitmentUserDetailsService();
     }
 
+    /**
+     * Decrypting passwords from db
+     *
+     * @return A BcryptPasswordEncoder
+     */
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-   /* @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("passwordd")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }*/
+    /**
+     * Handles authorization, depending on what role the user has the user shall be directed and be able to access different views
+     *
+     * @return Handler for authentication Success
+     */
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler() {
+        return new RecruitmentSimpleUrlAuthenticationSuccessHandler();
+    }
 }
